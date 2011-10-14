f748fe526580a2ee688c69b74ebcdd1560c2f298
Eugene Terehov <eugene@terehov.de>
2011-10-14 16:42:08 +0200

    first changes

--- assets/www/js/logic/tanks-logic-game.js
+++ assets/www/js/logic/tanks-logic-game.js
@@ -3,6 +3,7 @@ tanks.logic = tanks.logic || {};
 
 tanks.logic.game = (function(){
 
+	
 	var 
 		_sin = [],
 		_cos = []
@@ -16,10 +17,29 @@ tanks.logic.game = (function(){
 			}
 		})();	
 
+	// place the tank on the screen
+	tanks.events.register( "onRenderTank", "RENDER_TANK" );
+
+	
+	
+	// register Events
+	tanks.events.listenOnce(document, tanks.events.APP_READY, function(){
+
+		// render tank
+		tanks.events.trigger( document, tanks.events.RENDER_TANK);
+		
+		navigator.accelerometer.watchAcceleration(function(e){
+			console.log('X: ' + e.x + ' / Y: ' + e.y + " / Z: " + e.z);
+		}, function(){}, {frequency: 250});
+		
+	});
 	
 
 	return {
 		
 	};
+	
+	
+	
 	
 })();
--- assets/www/js/model/Tank.js
+++ assets/www/js/model/Tank.js
@@ -2,17 +2,33 @@ tanks = this.tanks || {};
 tanks.model = tanks.model || {};
 
 tanks.model.Tank = (function(){
-
+	
+	var _imgSource = "img/tank.png",
+        _top = 0,
+        _left = 0,
+        _angel= 0,
+        _speed= 0;
+	
+	
+	
+        /*
+    (function(){
+            var mp = Math.PI / 180;
+            for( var i=0; i < 361; ++i) {
+                _sin[i] = Math.sin(i*mp);
+                _cos[i] = Math.cos(i*mp);
+            }
+        })(); 
 	
 
 	var constr = function(id){
 	
-	};
+	};*/
 	
-	constr.prototype = 
+	/*constr.prototype = {
 			
-		constructor: constr
+		//constructor: constr
 	};
 
-	return constr;
+	return constr;*/
 })();
